b70df9e21aeb3cd0c46da0b516c76ee8aad08e47
Lucas Machado <lucasmachado@sisconsultoria.com.br>
2020-09-26 19:43:02 -0300

    first commit

---
+++ src/main/java/br/com/meutransporte/meutransporte/MeutransporteApplication.java
@@ -0,0 +1,13 @@
+package br.com.meutransporte.meutransporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeutransporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeutransporteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/meutransporte/meutransporte/MeutransporteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meutransporte.meutransporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeutransporteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
